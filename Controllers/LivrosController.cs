b631aa9c070e9611655d873b2ea2893aa3b67971
Luiz Fernando <LUIZFERNANDOJUNIOR9999@GMAIL.COM>
2026-07-23 21:06:42 -0300

    Incluídos os botoões de Delete e Edit

--- Controllers/LivrosController.cs
+++ Controllers/LivrosController.cs
@@ -7,8 +7,8 @@ namespace WebBiblioteca.Controllers {
         private static List<Livro> _livros = new List<Livro>();
 
         static LivrosController() {
-            _livros.Add(new Livro { Sequencial = 1, Titulo = "Dom Casmurro", Tombo = "avd56" }); 
-            _livros.Add(new Livro { Sequencial = 2, Titulo = "1984", Tombo = "dff67" });
+            _livros.Add(new Livro { Sequencial = 1, Titulo = "Dom Casmurro", Tombo = "avd56", Cod_Categoria = "Romance" });
+            _livros.Add(new Livro { Sequencial = 2, Titulo = "1984", Tombo = "dff67", Cod_Categoria = "Ação" });
         }
 
         public IActionResult Index() {
@@ -18,10 +18,10 @@ namespace WebBiblioteca.Controllers {
             //     new Livro {Sequencial = 1 , Titulo = "Dom Casmurro" , Tombo = "avd56"} ,
             //     new Livro {Sequencial = 2 , Titulo = "1984" , Tombo = "dff67"}
             //};
-                return View(_livros);
+            return View(_livros);
         }
-       
-    
+
+
         // GET: Livros/Create
         public IActionResult Create() {
             return View();
@@ -35,24 +35,76 @@ namespace WebBiblioteca.Controllers {
 
             //return Content("CHEGOU NO POST");
 
-            
-            //if (ModelState.IsValid)
-            //{
-                
-                livro.Sequencial = _livros.Any() ? _livros.Max(l =>  l.Sequencial) + 1 : 1;
-                _livros.Add(livro);
 
-                // Por enquanto só vamos simular salvando
-                return RedirectToAction("Index");
-                
+            if (!ModelState.IsValid)
+                return View(livro);
+
+            //{
+
+            livro.Sequencial = _livros.Any() ? _livros.Max(l => l.Sequencial) + 1 : 1;
+            _livros.Add(livro);
+
+            // Por enquanto só vamos simular salvando
+            return RedirectToAction("Index");
+
 
             //}
 
             //return View(livro);
 
             //return Content("MODEL OK");
-            
+
         }
 
+        public IActionResult Edit(int id) {
+            var livro = _livros.FirstOrDefault(l => l.Sequencial == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return View(livro);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Livro livro) {
+            if (!ModelState.IsValid)
+                return View(livro);
+            var existingLivro = _livros.FirstOrDefault(l => l.Sequencial == livro.Sequencial);
+            if (existingLivro == null)
+            {
+                return NotFound();
+            }
+            existingLivro.Titulo = livro.Titulo;
+            existingLivro.Tombo = livro.Tombo;
+            existingLivro.Cod_Categoria = livro.Cod_Categoria;
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Delete(int id) {
+
+            var livro = _livros.FirstOrDefault(l => l.Sequencial == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return View(livro);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Livro livro) {
+            var livroExistente = _livros.FirstOrDefault(l => l.Sequencial == livro.Sequencial);
+            if (livroExistente == null)
+            {
+                return NotFound();
+            }
+
+            if (livroExistente != null)
+            {
+                _livros.Remove(livroExistente);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/Livro.cs
+++ Models/Livro.cs
@@ -1,8 +1,17 @@
-﻿namespace WebBiblioteca.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBiblioteca.Models {
     public class Livro {
+
         public int Sequencial { get; set; }
+
+        [Required(ErrorMessage = "O Título é obrigatório!")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Tombo é obrigatório!")]
         public string Tombo { get; set; }
+        
+        [Required(ErrorMessage = "A Categoria é obrigatória!")]
         public string Cod_Categoria { get; set; }
     }
 }
